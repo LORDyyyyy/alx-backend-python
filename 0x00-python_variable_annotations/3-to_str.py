1df94400f852c8633be479bec01757f0b9e570b8
LORDyyyyy <mlord6271@gmail.com>
2024-01-11 15:30:17 +0200

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" to_str fucntion """
+
+
+def to_str(n: float) -> str:
+    """
+    a type-annotated function add that takes 1 float variable and returns
+    it's string representation.
+    """
+    return (str(n))
