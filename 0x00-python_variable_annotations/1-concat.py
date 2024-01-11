cc8d7caf03a6a9602c98095d190ac126daeeed7f
LORDyyyyy <mlord6271@gmail.com>
2024-01-11 15:13:53 +0200

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" concat fucntion """
+
+
+def concat(a: str, b: str) -> str:
+    """
+    a type-annotated function add that takes 2 strings variables and returns
+    concatenation.
+    """
+    return (a + b)
