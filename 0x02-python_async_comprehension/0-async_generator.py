f4d71d8e568ebbe2561903160a4c00a349ac1c81
LORDyyyyy <mlord6271@gmail.com>
2024-01-17 15:49:03 +0200

    task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,11 @@
 """ Async Comprehension """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import AsyncIterable
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncIterable[float]:
+    """ doc """
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
