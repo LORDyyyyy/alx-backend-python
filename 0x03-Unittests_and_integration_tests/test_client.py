d7bdfa7e10418277075006b2f6d1a04fcd2e490a
LORDyyyyy <mlord6271@gmail.com>
2024-02-05 20:33:03 +0200

    task 4

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,18 +2,21 @@
 """ Parameterize a unit test """
 import unittest
 from client import GithubOrgClient
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 from parameterized import parameterized
 
 
 class TestGithubOrgClient(unittest.TestCase):
     """ a unittest class for the  GithubOrgClient class """
 
-    @parameterized.expand([('google', {"login": "google"}), ('abc', {"message": "Not Found"})])
+    @parameterized.expand([
+        ('google', {"login": "google"}),
+        ('abc', {"message": "Not Found"})
+    ])
     @patch('client.get_json')
     def test_org(self, org_name, respond, mock_get_json):
         """ test org """
-        mock_get_json.return_value = respond
+        mock_get_json.return_value = Mock(return_value=respond)
         github_client = GithubOrgClient(org_name)
-        self.assertEqual(github_client.org(), mock_get_json.return_value)
+        self.assertEqual(github_client.org(), respond)
         mock_get_json.assert_called_once()
