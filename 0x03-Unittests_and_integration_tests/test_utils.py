c54a17cecb0344b67cd74f77962d33db5cf2674d
LORDyyyyy <mlord6271@gmail.com>
2024-02-05 20:03:13 +0200

    task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Parameterize a unit test """
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 import unittest
 from unittest.mock import Mock, patch
 from parameterized import parameterized
@@ -43,3 +43,26 @@ class TestGetJson(unittest.TestCase):
         res = get_json(test_url)
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(res, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """ a unittest class for the utils.memoize method """
+
+    class TestClass:
+
+        def a_method(self):
+            return 42
+
+        @memoize
+        def a_property(self):
+            return self.a_method()
+
+    def test_memoize(self):
+        """ test memoize """
+        with patch.object(self.TestClass, 'a_method') as mock_method:
+            test_instance = self.TestClass()
+            
+            test_instance.a_property()
+            test_instance.a_property()
+
+            mock_method.assert_called_once()
